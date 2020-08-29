c1f50785ae0ea136452dcd2bd0e24b43f78b4825
Ivan Sliepov <slepow1985@gmail.com>
2020-08-30 01:56:27 +0300

    Initial Commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Ivan Sliepov on 30.08.2020.
+//  Copyright © 2020 Ivan Sliepov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
